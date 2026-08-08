ec095ec96952fdfd60cac143005cac6c365e3aac
Onkar-Satale <onkarsatale4@gmail.com>
2026-08-08 15:37:45 +0530

    Add DB_SSL support for cloud database deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -11,7 +11,8 @@ export const db = mysql.createPool({
   port: Number(process.env.DB_PORT) || 3306,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ...(process.env.DB_SSL === "true" && { ssl: { rejectUnauthorized: false } })
 });
 
 export default db;
