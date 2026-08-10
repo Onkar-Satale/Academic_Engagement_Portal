464fec358ebe35a0043bcdf798e8969b461f2d08
Onkar-Satale <onkarsatale4@gmail.com>
2026-08-10 16:36:48 +0530

    Fix double slash in axios baseURL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const rawUrl = process.env.REACT_APP_BACKEND_URL || process.env.REACT_APP_API_URL || "http://localhost:5000";
-const cleanUrl = rawUrl.replace(/\/api\/?$/, "");
+const cleanUrl = rawUrl.replace(/\/+$/, "").replace(/\/api$/, "");
 
 const api = axios.create({
   baseURL: `${cleanUrl}/api`
